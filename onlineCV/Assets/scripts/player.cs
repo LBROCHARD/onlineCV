0a254cd52ecd30c4ce3e0575e09fe4daaa59986c
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2022-03-18 20:49:05 +0100

    [player.cs, SampleScene and added a bunch] added more correct ship and few fix to player

--- onlineCV/Assets/scripts/player.cs
+++ onlineCV/Assets/scripts/player.cs
@@ -7,9 +7,28 @@ public class player : MonoBehaviour
     public Rigidbody body;
     public Animator animator;
     public float speed = 6f;
+    public Camera mainCam;
+    public Camera cam2;
+    private bool canMove = true;
 
-    void Start(){}
-    void Update(){}
+    void Start()
+    {
+        mainCam.enabled = true;
+        cam2.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ChangeCamera();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = new Vector3( 0f, 1.5f, 0f);
+            transform.rotation = Quaternion.identity;
+        }
+    }
 
     void FixedUpdate()
     {
@@ -19,19 +38,27 @@ public class player : MonoBehaviour
 
         if(direction.magnitude >= 0.1f )
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            if (canMove == true) 
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
-            body.MovePosition( body.position + direction * speed * Time.fixedDeltaTime);
-            animator.SetBool("isMoving", true);
+                body.MovePosition( body.position + direction * speed * Time.fixedDeltaTime);
+                animator.SetBool("isMoving", true);
+            }
         }
         else
         {
             animator.SetBool("isMoving", false);
         }
-
     }
-
+    
+    void ChangeCamera()
+    {
+        mainCam.enabled = !mainCam.enabled;
+        cam2.enabled = !cam2.enabled;
+        canMove = !canMove;
+    }
 }
 
 // the basic values of the "input axis name" in the camera is : "Mouse Y" and "Mouse X"
