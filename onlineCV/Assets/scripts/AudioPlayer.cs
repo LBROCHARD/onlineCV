0850f9170364e2b83b93da3129bfb83a84afd49a
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2022-04-01 17:10:18 +0200

    [SampleScene.unity]added tv infos

---
+++ onlineCV/Assets/scripts/AudioPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    
+    void Start()
+    {
+        audioSource.Play();
+    }
+}
