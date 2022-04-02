0f833bfa84167b137b89061a698a6d4468817ead
chavantfou <73283488+LBROCHARD@users.noreply.github.com>
2022-04-02 18:44:08 +0200

    [SampleScene.unity, DisplayingObject.cs ] added most infos, redone the Displaying object script with array

--- onlineCV/Assets/scripts/DisplayingObject.cs
+++ onlineCV/Assets/scripts/DisplayingObject.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class DisplayingObject : ZoomableObject
 {
-
-    public Transform screen;
-    public Transform screen2;
+    public Transform[] screens;
 
     void Start()
     {
-        screen.GetComponent<Renderer>().enabled = false;
-        screen2.GetComponent<Renderer>().enabled = false;
+        foreach(Transform transf in screens)
+        {
+            transf.GetComponent<Renderer>().enabled = false;;
+        }
         focusCamera.enabled = false;
         zoomObject.GetComponent<Renderer>().enabled = false;
     }
@@ -24,8 +24,12 @@ public class DisplayingObject : ZoomableObject
             player player = other.gameObject.GetComponent<player>();
             player.inCollider = true;
             player.focusCamera = focusCamera;
-            screen.GetComponent<Renderer>().enabled = true;
-            screen2.GetComponent<Renderer>().enabled = true;
+
+            foreach(Transform transf in screens)
+            {
+                transf.GetComponent<Renderer>().enabled = true;
+            }
+
             zoomObject.GetComponent<Renderer>().enabled = true;
         }
     }
@@ -35,8 +39,12 @@ public class DisplayingObject : ZoomableObject
         if (other.gameObject.tag == "Player"){
             player player = other.gameObject.GetComponent<player>();
             player.inCollider = false;
-            screen.GetComponent<Renderer>().enabled = false;
-            screen2.GetComponent<Renderer>().enabled = false;
+
+            foreach(Transform transf in screens)
+            {
+                transf.GetComponent<Renderer>().enabled = false;
+            }
+            
             zoomObject.GetComponent<Renderer>().enabled = false;
         }
     }
